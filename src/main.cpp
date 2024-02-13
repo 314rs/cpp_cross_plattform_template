c00416f1ddd3d24c4e20dc93e5518c3df937490c
Lars <lars.borntraeger@gmx.de>
2024-02-13 04:13:12 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <sqlite3.h>
+#include <gtest/gtest.h>
+#include <boost/beast.hpp>
+
+int main(int argc, char const *argv[])
+{
+    
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
+
+
